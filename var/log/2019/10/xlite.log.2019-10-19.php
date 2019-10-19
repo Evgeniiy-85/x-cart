90a43ecdacc3d90442e7322ca52bc56c408f2cd5
e.ainulov <e.ainulov@pro100agency.ru>
2019-10-19 20:33:16 +0400

    Удалил логи

--- var/log/2019/10/xlite.log.2019-10-19.php
+++
@@ -1,7 +0,0 @@
-<?php die(1); ?>
-Oct 19 17:33:37 XLite [notice] Form ID checking failure (target: login, action: login)
-Runtime id: 9119a466fa298e0620b20a57271c32be;
-Server API: apache2handler; IP: 127.0.0.1;
-Request method: POST;
-URI: ?target=login&action=login;
-
